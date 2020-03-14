409c1a8a2d23b3de5295eb2b181511b055e033d9
john_hartley <jhartley@electricpen.com.au>
2020-03-14 16:27:54 +1100

    Reformatter object and unit tests (MS Test)

---
+++ PDFdivvyWF/Reformatter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDFdivvyWF
+{
+    public class Reformatter
+    {
+        public Reformatter()
+        {
+        }
+
+        public static string resolveHeaders(string format,string[] header)
+        {
+            for (int field = 0; field < header.Length; field++)
+                format = format.Replace("{" + header[field], "{" + field);
+            return format;
+        }
+    }
+}
---
+++ UnitTestDivvy/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTestDivvy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("UnitTestDivvy")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b7ab2213-78a0-4867-b538-cd2e0b2edd46")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
